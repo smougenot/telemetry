2afcdfda6b7df9e196ec3f1ff76aab8111d1bf33
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2019-10-13 02:12:10 +0200

    manage ressource a cleaner way

--- start_stack.sh
+++ start_stack.sh
@@ -5,31 +5,9 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
-#
-# Settings
-
-export DATA_DIR='/data/data'
-
-#
-# Code
-
 current_dir=$(cd -P -- "$(dirname -- "$BASH_SOURCE[0]")" && pwd -P)
 
-echo "ensure user exists"
-if [ $(getent passwd telemetry | wc -l) -lt 1 ]; then
-  echo "missing user telemetry"
-  sudo useradd --system --user-group --groups docker telemetry
-fi 
-
-echo "ensure data directory is initaited"
-for p  in "${DATA_DIR}/mosquitto/data" \
-    "${DATA_DIR}/mosquitto/log" \
-    "${DATA_DIR}/influxdb" \
-    "${DATA_DIR}/grafana"; do
-  [ -d "${p}" ] || mkdir -p "${p}"
-done
-
-echo "Startng stack"
+echo "Starting stack"
 docker stack deploy telemetry --compose-file "${current_dir}/compose.yml"
 
 docker stack services telemetry
