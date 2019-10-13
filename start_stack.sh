29e9d7c5a3bb76943954134c90ef3e9f623ce537
Sylvain Mougenot <sylvain.mougenot@gmail.com>
2019-10-14 01:03:16 +0200

    ensure local image are created

--- start_stack.sh
+++ start_stack.sh
@@ -7,6 +7,12 @@ set -o errexit   ## set -e : exit the script if any statement returns a non-true
 
 current_dir=$(cd -P -- "$(dirname -- "$BASH_SOURCE[0]")" && pwd -P)
 
+# ensure local images are built
+if [ $(doker images | grep -c 'node-red-with-influx:latest') -lt 1 ]; then
+  echo "Building node-red image"
+  docker build --file "${current_dir}/node-red/nodered-DockerFile.yml" --tag node-red-with-influx:latest .
+fi
+
 echo "Starting stack"
 docker stack deploy telemetry --compose-file "${current_dir}/compose.yml"
 
